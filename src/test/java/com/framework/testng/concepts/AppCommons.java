befc8229787017e61e06648d4eaa3b13e84fc07f
bharathautomation <soft.ittech20@gmail.com>
2025-03-25 21:04:24 +0530

    TestNG features

--- src/test/java/com/framework/testng/concepts/AppCommons.java
+++ src/test/java/com/framework/testng/concepts/AppCommons.java
@@ -5,18 +5,20 @@ import org.testng.annotations.Test;
 
 public class AppCommons extends WebCommons{
 	
-	@Test
+	@Test (groups= {"Regression"},invocationCount=10)
 	public void TestCase1() {
-		Assert.fail("Test Case 1 failed");
+	//	Assert.fail("Test Case 1 failed");
 		System.out.println("Test Case 1 - Home Page Executed Successfully...");
 	}
 	
-	@Test
-	public void TestCase2() {
+	@Test (priority=2,groups= {"Regression","Sanity"}, timeOut=2000)
+	public void TestCase2() throws InterruptedException {
+		Thread.sleep(3000);
 		System.out.println("Test Case 2 - Add Product to Cart Feature Executed Successfully...");
+		
 	}	
 
-	@Test(dataProvider="testcase3")
+	@Test(dataProvider="testcase3", priority=-1,groups= {"Regression","Sanity","Smoke"},dependsOnMethods={"TestCase2","TestCase2"}, enabled=true)
 	public void TestCase3(String username, String password) {
 		System.out.println("Test Case 3 - Login Executed Successfully with..."+username+" ,"+password);
 	}
--- src/test/java/com/framework/testng/concepts/WebCommons.java
+++ src/test/java/com/framework/testng/concepts/WebCommons.java
@@ -9,24 +9,36 @@ import org.testng.annotations.Parameters;
 
 public class WebCommons {
 	
-	@BeforeSuite
+	@BeforeSuite(alwaysRun=true)
 	public void startReporting() {
 		System.out.println("Reporting process started.....");
 	}
 	
-	@AfterSuite
+	@AfterSuite(alwaysRun=true)
 	public void stopReporting() {
 		System.out.println("Reporting process stopped and Report generated.....");
 	}
 	
-	@BeforeMethod
+//	@BeforeMethod(alwaysRun=true)
+//	@Parameters({"Browser","URL"})
+//	public void launchBrowserAndApplication(String browser, String url) {
+//		System.out.println(browser+" is launched...");
+//		System.out.println(url+" is launched...");
+//	}
+	
+	@BeforeMethod(alwaysRun=true)
 	@Parameters({"Browser","URL"})
-	public void launchBrowserAndApplication(String browser, String url) {
+	public void launchBrowser(String browser, String url) {
 		System.out.println(browser+" is launched...");
+	}
+	
+	@BeforeMethod(alwaysRun=true, dependsOnMethods="launchBrowser")
+	@Parameters({"Browser","URL"})
+	public void launchApplication(String browser, String url) {
 		System.out.println(url+" is launched...");
 	}
 	
-	@AfterMethod
+	@AfterMethod(alwaysRun=true)
 	public void closeBrowser() {
 		System.out.println("Browser is closed....");
 	}
